acb5f1f1da62f831bc9a1ba8803f4b97fa02f673
wen-wen520 <wen_wen520@outlook.com>
2025-07-12 23:03:55 +0800

    Re indent with tab
    Signed-off-by: wen-wen520 <wen_wen520@outlook.com>

--- src/client/java/wen_wen520/magpiebridge/client/core/Monitor.java
+++ src/client/java/wen_wen520/magpiebridge/client/core/Monitor.java
@@ -12,61 +12,61 @@ import static wen_wen520.magpiebridge.client.utils.Initializer.dir_defaultHead;
 
 public class Monitor implements ClientModInitializer {
 
-    @Override
-    public void onInitializeClient() {
-        // Listen for chat messages
-        ClientReceiveMessageEvents.CHAT.register((message, signedMessage, sender, params, _temp) -> {
+	@Override
+	public void onInitializeClient() {
+		// Listen for chat messages
+		ClientReceiveMessageEvents.CHAT.register((message, signedMessage, sender, params, _temp) -> {
 
-            String chatText = message.getString();
-            MessageParser.ParsedMessage parsed = MessageParser.parse(chatText);
-            String senderName = sender.getName();
-            String headPath = dir_defaultHead.getAbsolutePath();
+			String chatText = message.getString();
+			MessageParser.ParsedMessage parsed = MessageParser.parse(chatText);
+			String senderName = sender.getName();
+			String headPath = dir_defaultHead.getAbsolutePath();
 
-            try{
-                PlayerHeadExporter.fetchAndSavePlayerHeadAsync(senderName).thenAccept(path -> {
-                    WindowsNotificationHelper.sendNotification("Minecraft", senderName, parsed.msg, path);
-                });
-            }
-            catch (Exception e) {
-                System.err.println("Failed to fetch player head for " + senderName + ": " + e.getMessage());
-                WindowsNotificationHelper.sendNotification("Minecraft", senderName, parsed.msg, headPath);
-            }
+			try{
+				PlayerHeadExporter.fetchAndSavePlayerHeadAsync(senderName).thenAccept(path -> {
+					WindowsNotificationHelper.sendNotification("Minecraft", senderName, parsed.msg, path);
+				});
+			}
+			catch (Exception e) {
+				System.err.println("Failed to fetch player head for " + senderName + ": " + e.getMessage());
+				WindowsNotificationHelper.sendNotification("Minecraft", senderName, parsed.msg, headPath);
+			}
 
-        });
+		});
 
-        ClientReceiveMessageEvents.GAME.register((message, overlay) ->{
+		ClientReceiveMessageEvents.GAME.register((message, overlay) ->{
 
-            if (overlay) {
-                return;
-            }
+			if (overlay) {
+				return;
+			}
 
-            String headPath = dir_defaultHead.getAbsolutePath();
-            String chatText = GeneralFilter.filterMessage(message.getString());
-            String[] NotificationMsg = null;
+			String headPath = dir_defaultHead.getAbsolutePath();
+			String chatText = GeneralFilter.filterMessage(message.getString());
+			String[] NotificationMsg = null;
 
-            // Skyblock
-            if (NotificationMsg == null) {
-                NotificationMsg = SkyBlockMsgFilter.filterMessage(chatText);
-            }
-            // Zombie
-            if (NotificationMsg == null){
-                NotificationMsg = ZombieMsgFilter.filterMessage(chatText);
-            }
-            if (NotificationMsg == null) {
-                return;
-            }
+			// Skyblock
+			if (NotificationMsg == null) {
+				NotificationMsg = SkyBlockMsgFilter.filterMessage(chatText);
+			}
+			// Zombie
+			if (NotificationMsg == null){
+				NotificationMsg = ZombieMsgFilter.filterMessage(chatText);
+			}
+			if (NotificationMsg == null) {
+				return;
+			}
 
-            try{
-                String[] finalNotificationMsg = NotificationMsg;
-                PlayerHeadExporter.fetchAndSavePlayerHeadAsync(NotificationMsg[1]).thenAccept(path -> {
-                    WindowsNotificationHelper.sendNotification(finalNotificationMsg[0], finalNotificationMsg[1], finalNotificationMsg[2], path);
-                });
-            }
-            catch (Exception e) {
-                System.err.println("Failed to fetch player head for " + NotificationMsg[1] + ": " + e.getMessage());
-                WindowsNotificationHelper.sendNotification(NotificationMsg[0], NotificationMsg[1], NotificationMsg[2], headPath);
-            }
+			try{
+				String[] finalNotificationMsg = NotificationMsg;
+				PlayerHeadExporter.fetchAndSavePlayerHeadAsync(NotificationMsg[1]).thenAccept(path -> {
+					WindowsNotificationHelper.sendNotification(finalNotificationMsg[0], finalNotificationMsg[1], finalNotificationMsg[2], path);
+				});
+			}
+			catch (Exception e) {
+				System.err.println("Failed to fetch player head for " + NotificationMsg[1] + ": " + e.getMessage());
+				WindowsNotificationHelper.sendNotification(NotificationMsg[0], NotificationMsg[1], NotificationMsg[2], headPath);
+			}
 
-        });
-    }
+		});
+	}
 }
--- src/client/java/wen_wen520/magpiebridge/client/core/WindowsNotificationHelper.java
+++ src/client/java/wen_wen520/magpiebridge/client/core/WindowsNotificationHelper.java
@@ -7,20 +7,20 @@ import static wen_wen520.magpiebridge.client.utils.Initializer.dir_bridge;
 public class WindowsNotificationHelper {
 
 
-    public static void sendNotification(String app, String title, String message, String iconPath) {
-        ProcessBuilder pb = new ProcessBuilder(
-                dir_bridge.getAbsolutePath(),
-                "-app-id", app,
-                "-t", title,
-                "-m", message,
-                "-icon", iconPath,
-                "-audio", "sms",
-                "duration", "short"
-        );
-        try {
-            pb.start();
-        } catch (IOException e) {
-            System.err.println("Failed to send notification: " + e.getMessage());
-        }
-    }
+	public static void sendNotification(String app, String title, String message, String iconPath) {
+		ProcessBuilder pb = new ProcessBuilder(
+				dir_bridge.getAbsolutePath(),
+				"-app-id", app,
+				"-t", title,
+				"-m", message,
+				"-icon", iconPath,
+				"-audio", "sms",
+				"duration", "short"
+		);
+		try {
+			pb.start();
+		} catch (IOException e) {
+			System.err.println("Failed to send notification: " + e.getMessage());
+		}
+	}
 }
