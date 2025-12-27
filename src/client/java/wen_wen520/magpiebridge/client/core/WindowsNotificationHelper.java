dba242eab89ece3432e9d21f8b7b043f02e1f66c
wen <94116517+wen-wen520@users.noreply.github.com>
2025-12-27 20:07:07 +0000

    Resolve UTF-8 encoding garbled text

--- src/client/java/wen_wen520/magpiebridge/client/core/WindowsNotificationHelper.java
+++ src/client/java/wen_wen520/magpiebridge/client/core/WindowsNotificationHelper.java
@@ -1,25 +1,23 @@
 package wen_wen520.magpiebridge.client.core;
 
 import java.io.IOException;
-
 import static wen_wen520.magpiebridge.client.utils.Initializer.dir_bridge;
 
 public class WindowsNotificationHelper {
-
-
 	public static void sendNotification(String app, String title, String message, String iconPath) {
-		ProcessBuilder pb = new ProcessBuilder(
+		ProcessBuilder builder = new ProcessBuilder(
 				dir_bridge.getAbsolutePath(),
 				"-app-id", app,
-				"-t", title,
-				"-m", message,
+				"-title", title,
+				"-message", message,
 				"-icon", iconPath,
 				"-audio", "sms",
-				"duration", "short"
+				"-duration", "short"
 		);
 		try {
-			pb.start();
-		} catch (IOException e) {
+			builder.start();
+		} 
+		catch (IOException e) {
 			System.err.println("Failed to send notification: " + e.getMessage());
 		}
 	}
